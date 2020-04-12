0f541ef95b16ae9d33715ca2ff9c696a11e8668f
Manuel <Manuel@DESKTOP-5EP51LA>
2020-04-12 13:56:13 +0200

    Envio de email y recaptcha Ok, modificado select de idioma Falta la validacion del form-contact con parsleyJS

--- themes/meghna-hugo/assets/js/contact.js
+++ themes/meghna-hugo/assets/js/contact.js
@@ -2,15 +2,30 @@
     emailjs.init('user_2T3vXgPtAE1omvimOM7zl');
 })();
 
-window.onload = function () {
-    document.getElementById('contact-form').addEventListener('submit', function (event) {
+$(document).ready(function(){
+    let succes = '<div id="alert-succes" class="alert alert-primary alert-dismissible fade show" role="alert">'+
+                 $('#alert-succes').html()+
+                 '<button type="button" class="close" data-dismiss="alert" aria-label="Close">'+
+                 '<span aria-hidden="true">&times;</span>'+
+                 '</button>'+
+                 '</div>';
+    let error = '<div id="alert-error" class="alert alert-danger alert-dismissible fade show" role="alert">'+
+                 $('#alert-error').html()+
+                 '<button type="button" class="close" data-dismiss="alert" aria-label="Close">'+
+                 '<span aria-hidden="true">&times;</span>'+
+                 '</button>'+
+                 '</div>';
+
+    $("#contact-form").submit(function (event) {
         event.preventDefault();
-        emailjs.sendForm('gmail', 'contact_form', this).then(function(response) {
-            document.getElementById('contact-form').reset();
-            //document.getElementById('alert').classList.add("show");
-            location.reload();
-         }, function(error) {
-            console.log('FAILED...', error);
-         });
+        emailjs.sendForm('gmail', 'contact_form', this).then(function () {
+            $('#contact-form')[0].reset();
+            $('#alert').html(succes);
+            grecaptcha.reset();
+        }, function (error) {
+            $('#contact-form')[0].reset();
+            $('#alert').html(error);
+            grecaptcha.reset();
+        });
     });
-}
+});
