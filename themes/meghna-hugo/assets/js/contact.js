7de493ce1b0e45983c9388a81d5d9b85f07a234f
Manuel <Manuel@DESKTOP-5EP51LA>
2020-04-13 12:03:44 +0200

    Validacion del formulario, formateo de codigo de todos los ficheros

--- themes/meghna-hugo/assets/js/contact.js
+++ themes/meghna-hugo/assets/js/contact.js
@@ -2,19 +2,19 @@
     emailjs.init('user_2T3vXgPtAE1omvimOM7zl');
 })();
 
-$(document).ready(function(){
-    let succes = '<div id="alert-succes" class="alert alert-primary alert-dismissible fade show" role="alert">'+
-                 $('#alert-succes').html()+
-                 '<button type="button" class="close" data-dismiss="alert" aria-label="Close">'+
-                 '<span aria-hidden="true">&times;</span>'+
-                 '</button>'+
-                 '</div>';
-    let error = '<div id="alert-error" class="alert alert-danger alert-dismissible fade show" role="alert">'+
-                 $('#alert-error').html()+
-                 '<button type="button" class="close" data-dismiss="alert" aria-label="Close">'+
-                 '<span aria-hidden="true">&times;</span>'+
-                 '</button>'+
-                 '</div>';
+$(document).ready(function () {
+    let succes = '<div id="alert-succes" class="alert alert-primary alert-dismissible fade show" role="alert">' +
+            $('#alert-succes').html() +
+            '<button type="button" class="close" data-dismiss="alert" aria-label="Close">' +
+            '<span aria-hidden="true">&times;</span>' +
+            '</button>' +
+            '</div>';
+    let error = '<div id="alert-error" class="alert alert-danger alert-dismissible fade show" role="alert">' +
+            $('#alert-error').html() +
+            '<button type="button" class="close" data-dismiss="alert" aria-label="Close">' +
+            '<span aria-hidden="true">&times;</span>' +
+            '</button>' +
+            '</div>';
 
     $("#contact-form").submit(function (event) {
         event.preventDefault();
@@ -22,7 +22,7 @@ $(document).ready(function(){
             $('#contact-form')[0].reset();
             $('#alert').html(succes);
             grecaptcha.reset();
-        }, function (error) {
+        }, function (e) {
             $('#contact-form')[0].reset();
             $('#alert').html(error);
             grecaptcha.reset();
