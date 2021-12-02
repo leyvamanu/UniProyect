4c875f3bc5469d34915b86ca053c5737f7cd3000
josan <josanescod@gmail.com>
2021-12-02 07:05:12 +0100

    Mostrar fecha actual en el footer

--- docs/plugins/custom/custom.js
+++ docs/plugins/custom/custom.js
@@ -1,11 +1,15 @@
-
 /*workaround para minimizar el menú del boton del movil 
 cuando se selecciona una opcion*/
 
-$( document ).ready(function() {
-    console.log( "testing..." );
-$('.navbar-nav>li>a').on('click', function(){
-    $('.navbar-collapse').collapse('hide');
-});
+$(document).ready(function () {
+  console.log("testing...");
+  $(".navbar-nav>li>a").on("click", function () {
+    $(".navbar-collapse").collapse("hide");
+  });
 
+  // mostrar fecha actual
+  let pDate = document.querySelector(".copyright");
+  const date = new Date().getFullYear();
+  pDate.children[02].innerHTML = `<p>Copyright © ${date} <a href="https://www.uniproyect.com">Uniproyect</a> todos los derechos reservados.</p>`;
+  ('<p>Copyright © 2021 <a href="https://www.uniproyect.com">Uniproyect</a> todos los derechos reservados.</p>');
 });
